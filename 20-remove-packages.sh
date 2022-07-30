3c6e0b81b4bd82890b715f1b797619022e7b1f8c
Cristiano Nunes <cfgnunes@gmail.com>
2022-07-30 09:29:02 -0300

    Fixed bug in Totem to play videos

--- 20-remove-packages.sh
+++ 20-remove-packages.sh
@@ -5,5 +5,8 @@ set -eu
 apt-get -y purge rhythmbox*
 apt-get -y purge thunderbird*
 
+# FIX: the bug in application Totem "The specified movie could not be found."
+apt-get -y purge gstreamer1.0-vaapi
+
 # Remove others packages
 apt-get -y --purge autoremove
