e661a7022847efc5a57aa77b7eda1e80c5d73137
Cristiano Nunes <cfgnunes@gmail.com>
2022-08-23 14:03:03 -0300

    Fix removing 'gstreamer1.0-vaapi'

--- 20-remove-packages.sh
+++ 20-remove-packages.sh
@@ -14,11 +14,5 @@ _remove_package firefox
 _remove_package rhythmbox
 _remove_package thunderbird
 
-# FIX: the bug in application Totem "The specified movie could not be found."
-_remove_package gstreamer1.0-vaapi
-
-# FIX: problem with Canon Scanner in Ubuntu 20.04
-_remove_package ippusbxd
-
 # Remove others packages
 apt-get -y --purge autoremove
