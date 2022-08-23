65b0a74fff35534f9d14b4a4c1765768b8aba86c
Cristiano Nunes <cfgnunes@gmail.com>
2022-08-23 14:03:57 -0300

    Code refactor

--- 20-remove-packages.sh
+++ 20-remove-packages.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-_remove_package() {
-    local PACKAGE="$1"
-
-    if dpkg -s "$PACKAGE" &>/dev/null; then
-        apt-get -y purge "$PACKAGE"*
-    fi
-}
-
 # Remove unused packages
-_remove_package firefox
-_remove_package rhythmbox
-_remove_package thunderbird
+apt-get -y purge firefox
+apt-get -y purge rhythmbox
+apt-get -y purge thunderbird
 
 # Remove others packages
 apt-get -y --purge autoremove
--- 70-optimizations.sh
+++ 70-optimizations.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-_remove_package() {
-    local PACKAGE="$1"
-
-    if dpkg -s "$PACKAGE" &>/dev/null; then
-        apt-get -y purge "$PACKAGE"*
-    fi
-}
-
 # Adjust swappiness: uses 90% of RAM before Linux kernel begins swapping
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 
@@ -25,7 +17,7 @@ sed -i "s|#DefaultTimeoutStopSec=90s|DefaultTimeoutStopSec=10s|" /etc/systemd/sy
 sed -i 's/rights="none" pattern="PDF"/rights="read|write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
 
 # FIX: the bug in application Totem "The specified movie could not be found."
-_remove_package gstreamer1.0-vaapi
+apt-get -y purge gstreamer1.0-vaapi
 
 # FIX: problem with Canon Scanner in Ubuntu 20.04
-_remove_package ippusbxd
+apt-get -y purge ippusbxd
