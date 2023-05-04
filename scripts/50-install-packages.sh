e84eeffb0744a2e924551b8ad3eea5db67765501
Cristiano Fraga G. Nunes <cfgnunes@gmail.com>
2023-05-04 14:50:52 -0300

    Update 50-install-packages.sh

--- scripts/50-install-packages.sh
+++ scripts/50-install-packages.sh
@@ -67,8 +67,8 @@ _install_apt aptitude
 _install_apt aria2
 _install_apt evtest
 _install_apt findimagedupes
-_install_apt htop
 _install_apt hexedit
+_install_apt htop
 _install_apt jpegoptim
 _install_apt lm-sensors
 _install_apt optipng
