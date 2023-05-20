fbd8f9fc807cea2fc97681afe3f195758bd00c36
Cristiano Fraga G. Nunes <cfgnunes@gmail.com>
2023-05-20 12:17:42 -0300

    Update 55-install-packages-external.sh

--- scripts/55-install-packages-external.sh
+++ scripts/55-install-packages-external.sh
@@ -9,7 +9,7 @@ echo " > Installing packages..."
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 #echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >/etc/apt/sources.list.d/google-chrome.list
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/google.gpg
-echo "deb [arch=$ARCH] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
 apt-get update
 apt-get -y install google-chrome-stable
 
