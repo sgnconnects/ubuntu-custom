2456fd85f963b1c7638386260df4b4d0e053cc7c
Cristiano Nunes <cfgnunes@gmail.com>
2023-05-22 12:08:07 -0300

    Added 'audacious' config

--- scripts/80-desktop-settings.sh
+++ scripts/80-desktop-settings.sh
@@ -4,26 +4,21 @@ set -eu
 echo "Script: 80-desktop-settings.sh"
 
 echo " > Setting a 'gnome-mimeapps.list' file..."
-cp data/gnome-mimeapps.list /usr/share/applications/
-chmod 644 /usr/share/applications/gnome-mimeapps.list
-chown root:root /usr/share/applications/gnome-mimeapps.list
+install -o root -m 644 data/gnome-mimeapps.list /usr/share/applications/gnome-mimeapps.list
 
 echo " > Installing new wallpapers..."
-cp data/win-dark.jpg /usr/share/backgrounds/
-chmod 644 /usr/share/backgrounds/win-dark.jpg
-chown root:root /usr/share/backgrounds/win-dark.jpg
-
-cp data/win-light.jpg /usr/share/backgrounds/
-chmod 644 /usr/share/backgrounds/win-light.jpg
-chown root:root /usr/share/backgrounds/win-light.jpg
-
-cp data/ubuntu-custom-wallpapers.xml /usr/share/gnome-background-properties/
-chmod 644 /usr/share/gnome-background-properties/ubuntu-custom-wallpapers.xml
-chown root:root /usr/share/gnome-background-properties/ubuntu-custom-wallpapers.xml
+install -o root -m 644 data/win-dark.jpg /usr/share/backgrounds/win-dark.jpg
+install -o root -m 644 data/win-light.jpg /usr/share/backgrounds/win-light.jpg
+install -o root -m 644 data/ubuntu-custom-wallpapers.xml /usr/share/gnome-background-properties/ubuntu-custom-wallpapers.xml
 
 echo " > Setting new desktop configuration..."
-cp data/90_ubuntu-custom.gschema.override /usr/share/glib-2.0/schemas/
-chmod 644 /usr/share/glib-2.0/schemas/90_ubuntu-custom.gschema.override
-chown root:root /usr/share/glib-2.0/schemas/90_ubuntu-custom.gschema.override
+install -o root -m 644 data/90_ubuntu-custom.gschema.override /usr/share/glib-2.0/schemas/90_ubuntu-custom.gschema.override
 
 glib-compile-schemas /usr/share/glib-2.0/schemas
+
+echo " > Setting app default configs..."
+mkdir -p /etc/skel/.config/
+cp -r data/config/* /etc/skel/.config/
+chmod -R 644 /etc/skel/.config/
+find /etc/skel/.config/ -type f -exec chmod 644 {} \;
+find /etc/skel/.config/ -type d -exec chmod 755 {} \;
