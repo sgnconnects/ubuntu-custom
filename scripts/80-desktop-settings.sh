14dd8d714d1f7f9f4bd84b25c1fdc2c6decd4dab
Cristiano Nunes <cfgnunes@gmail.com>
2023-06-03 13:54:50 -0300

    Rename files

--- scripts/80-desktop-settings.sh
+++ scripts/80-desktop-settings.sh
@@ -13,7 +13,7 @@ update-desktop-database
 echo " > Installing new wallpapers..."
 install -o root -m 644 data/windows-dark.jpg /usr/share/backgrounds/windows-dark.jpg
 install -o root -m 644 data/windows-light.jpg /usr/share/backgrounds/windows-light.jpg
-install -o root -m 644 data/ubuntu-custom-wallpapers.xml /usr/share/gnome-background-properties/ubuntu-custom-wallpapers.xml
+install -o root -m 644 data/windows-wallpapers.xml /usr/share/gnome-background-properties/windows-wallpapers.xml
 
 echo " > Setting new desktop configuration..."
 install -o root -m 644 data/90_ubuntu-custom.gschema.override /usr/share/glib-2.0/schemas/90_ubuntu-custom.gschema.override
