400f08fc9b904edd332ff40d6772dd900a50474e
Cristiano Nunes <cfgnunes@gmail.com>
2023-06-03 13:56:23 -0300

    Refactor

--- scripts/80-desktop-settings.sh
+++ scripts/80-desktop-settings.sh
@@ -6,24 +6,24 @@ echo "Script: 80-desktop-settings.sh"
 # Configuration based on:
 # grep -r "mime-type[ ]*type" | cut -d "=" -f 2 | sed "s|\">||" | sed "s|\"||" | sort --unique
 # on directory /usr/share/mime/packages
-echo " > Setting a 'mimeapps.list' file..."
+echo " > Setting mimeapps file..."
 install -o root -m 644 data/mimeapps.list /etc/xdg/mimeapps.list
 update-desktop-database
 
-echo " > Installing new wallpapers..."
+echo " > Installing wallpapers..."
 install -o root -m 644 data/windows-dark.jpg /usr/share/backgrounds/windows-dark.jpg
 install -o root -m 644 data/windows-light.jpg /usr/share/backgrounds/windows-light.jpg
 install -o root -m 644 data/windows-wallpapers.xml /usr/share/gnome-background-properties/windows-wallpapers.xml
 
-echo " > Setting new desktop configuration..."
+echo " > Setting desktop configuration..."
 install -o root -m 644 data/90_ubuntu-custom.gschema.override /usr/share/glib-2.0/schemas/90_ubuntu-custom.gschema.override
 glib-compile-schemas /usr/share/glib-2.0/schemas
 
-echo " > Setting new mime types..."
+echo " > Setting default mime types..."
 install -o root -m 644 data/mime/*.xml /usr/share/mime/packages/
 update-mime-database /usr/share/mime
 
-echo " > Setting app default configs..."
+echo " > Setting apps default configs..."
 cp -r data/skel/. /etc/skel/
 find /etc/skel/ -type f -exec chmod 644 {} \;
 find /etc/skel/ -type d -exec chmod 755 {} \;
