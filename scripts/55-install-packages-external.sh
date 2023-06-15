194a0a04c90427adeae0eb646d209f63bbff8c08
Cristiano Nunes <cfgnunes@gmail.com>
2023-06-15 14:32:37 -0300

    Small fix

--- scripts/55-install-packages-external.sh
+++ scripts/55-install-packages-external.sh
@@ -6,7 +6,7 @@ echo "Script: 55-install-packages-external.sh"
 echo " > Installing packages..."
 
 # Install Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --batch --yes --dearmour -o /etc/apt/trusted.gpg.d/google.gpg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --batch --yes --dearmour -o /etc/apt/trusted.gpg.d/google-chrome.gpg
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
 apt-get update
 apt-get -y install google-chrome-stable
